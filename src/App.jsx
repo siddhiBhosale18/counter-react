633856a61dc253d056597df97267d8e4d9cac8d6
siddhiBhosale18 <siddhibhosale1830@gmail.com>
2024-02-18 01:15:47 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
+import { useState } from 'react' //for hook
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [counter, setCount] = useState(1)
+
+  // let counter= 15;
+
+  const addValue=()=>{
+    
+    // counter=counter+1; 
+    setCount(counter+1);
+    console.log("value Added",counter); 
+  }
+
+  const removeValue=()=>{
+    setCount(counter - 1 );
+  }
 
   return (
     <>
-      <h1>heyy, it's REACT</h1>
+      <h1>Heyy, it's REACT</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add Value : {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value: {counter}</button>
     </>
   )
 }
